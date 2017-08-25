8bc82c4bd4ddb63b2809c401a5125d3bb0646121
Hayreddin Ciner <hayreddin.ciner@stud-mail.uni-wuerzburg.de>
2017-08-25 20:41:37 +0200

    SecAppWrapper: -- added JWT for verification (and changed old message handling) -- fixed bug where restarting the Wrapper resulted in falsified Wrapper IDs, and /keep-alive returned "Wrapper not registered" -- added SIGTERM handler to shutdown Wrapper gracefully and stop running thread of keep-alive.

--- SecAppWrapper/SAWrapper.py
+++ SecAppWrapper/SAWrapper.py
@@ -1,5 +1,5 @@
 # external Libs
-import netifaces
+import netifaces, jwt
 from SecAppWrapper import ApiURI
 # Standard Libs
 from urllib.request import Request, urlopen
@@ -10,7 +10,8 @@ import json, time
 # TODO: to implement /unregister daemonize the script.
 
 class Wrapper():
-        regTimeout = 60 # add to Argument list of constructor/init?
+        regTimeout = 5 # add to Argument list of constructor/init?
+        secret = "5566F08A9218BEAF6E6714B5870161CBD12A975F6E516FAD66D28FE56302930D"
         def __init__(self, saGroup, saControllerURL, saIface):
             self.logger = logging.getLogger('SecAppWrapper.SAWrapper')
             self.logger.info("[INIT] Initiating new Wrapper Instance")
@@ -18,17 +19,22 @@ class Wrapper():
             self.controllerURL = saControllerURL
             self.ready = False
             self.instanceID = None
+            self.token = None
             if(saIface == "default"):
                 self.iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
             else:
                 self.iface = saIface
             self.iface_mac = netifaces.ifaddresses('{0}'.format(self.iface))[netifaces.AF_LINK][0]['addr']
-            self.logger.info("[INIT] Aquired HW_ADDR for interface {0}: {1}".format(self.iface, self.iface_mac))
-            self.logger.info("[INIT] Sending Register Request to Controller... ", str(self.iface_mac))
+            self.logger.info("[INIT] Aquired HW_ADDR for interface %s: %s", self.iface, self.iface_mac)
+            self.logger.info("[INIT] Sending Register Request to Controller... %s", self.iface_mac)
             connected = False
             # { "type": "REGISTER", "group": "saGroup", "hw_addr": "mac-address", "token": "secureToken", "misc": "misc info" }
-            data = {'type': ApiURI.Type.REGISTER.name, 'group': self.group, 'hw_addr': self.iface_mac, 'token': '', 'misc': ''}
-            jsonData = json.dumps(data)
+            data = {'type': ApiURI.Type.REGISTER.name, 'group': self.group, 'hw_addr': self.iface_mac, 'misc': '', 'exp': int(time.time()+5*60)}
+            regToken = jwt.encode(data, Wrapper.secret, algorithm='HS256')
+            self.logger.info("[INIT] JWT generated: %s", str(regToken.decode("utf-8")))
+            regTokenJ = {"token": regToken.decode("utf-8")}
+            self.logger.info("[INIT] regTokenJ: %s", regTokenJ)
+            jsonData = json.dumps(regTokenJ)
             while(connected == False):
                 conn = Request(self.controllerURL+ApiURI.Type.REGISTER.value, jsonData.encode("utf-8"), {'Content-Type': 'application/json'})
                 try:
@@ -36,7 +42,10 @@ class Wrapper():
                     if(resp.getcode() == 200):
                         self.logger.info("[INIT] Connection successful")
                         respData = json.loads(resp.read().decode("utf-8"))
-                        self.instanceID = respData['instanceID']
+                        payload = jwt.decode(respData["token"], Wrapper.secret, algorithms=['HS256'])
+                        print(payload)
+                        self.instanceID = payload['instanceID']
+                        self.token = respData["token"]
                         connected = True
                         self.ready = True
                         self.logger.info("[INIT] Wrapper Instance registered with Instance ID: {0}".format(self.instanceID))
@@ -58,19 +67,21 @@ class Wrapper():
 
         def keepalive(self):
             kaData = {'type': ApiURI.Type.KEEPALIVE.name, 'name': self.instanceID,
-                      'group': self.group, 'hw_addr': self.iface_mac,
-                      'token': '', 'misc': ''}
+                      'group': self.group, 'hw_addr': self.iface_mac, 'misc': ''}
             jsonKaData = json.dumps(kaData)
             while (self.ready):
+                if(self.ready == False): break
                 self.logger.info("[KeepAlive] Waiting {0} seconds...".format(str(self.regTimeout)))
                 time.sleep(self.regTimeout)
                 self.logger.info("[KeepAlive] Initializing connection to Controller...")
                 kaConn = Request(self.controllerURL + ApiURI.Type.KEEPALIVE.value,
                                  jsonKaData.encode("utf-8"),
-                                 {'Content-Type': 'application/json'})
+                                 {'Content-Type': 'application/json', 'Authorization': "Bearer {0}".format(self.token)})
                 kaResp = urlopen(kaConn)
                 if (kaResp.getcode() == 200):
                     self.logger.info("[KeepAlive] Keep-Alive successfully send! Closing conenction...")
+                    respData = json.loads(kaResp.read().decode("utf-8"))
+                    self.token = respData["token"]
                     kaResp.close()
                 elif (kaResp.getcode() == 500):
                     self.logger.warning("[KeepAlive] Controller not available, retrying...")
@@ -80,4 +91,5 @@ class Wrapper():
                     self.logger.warning("[KeepAlive] Failed to send keep-alive")
                     # TODO what then?
                     break
+            if(self.ready == False): print("Thank you...")
             return 1
--- startWrapper.py
+++ startWrapper.py
@@ -1,8 +1,8 @@
 # external libs
-from flask import Flask, json, Response, make_response, request
+from flask import Flask, json, Response, make_response, request, jsonify
 import flask.logging
 from SecAppWrapper import ApiURI, SAWrapper
-import requests
+import requests, jwt
 
 # Standard Libs
 from urllib.request import urlopen, Request
@@ -12,9 +12,9 @@ app = Flask(__name__)
 wrapperInstance = None
 
 def main(argv):
-    group = None
-    controllerURL = None
-    iface = "default"
+    main.group = None
+    main.controllerURL = None
+    main.iface = "default"
     try:
         opts, args = getopt.getopt(argv, "vhg:u:i:", ["group=", "url=", "iface=", "verbose="])
     except getopt.GetoptError:
@@ -34,11 +34,11 @@ def main(argv):
             print("\tstartWrapper.py --group groupname --url controllerURL [--iface interface] [--verbose]")
             sys.exit(0)
         elif(opt in ("-g", "--group")):
-            group = arg
+            main.group = arg
         elif(opt in ("-u", "--url")):
             try:
                 r = requests.head(arg)
-                controllerURL = arg
+                main.controllerURL = arg
             except requests.exceptions.InvalidURL:
                 print("Invalid URL.")
                 sys.exit(0)
@@ -46,25 +46,23 @@ def main(argv):
                 print("Server not available. Check URL: ", arg)
                 sys.exit(0)
         elif(opt in ("-i", "--iface")):
-            iface = arg
-    if(group == None or controllerURL == None):
+            main.iface = arg
+    if(main.group == None or main.controllerURL == None):
         print("startWrapper.py\t---- illegal options.\nSee startWrapper.py -h for help")
         sys.exit(0)
     #print("Starting Wrapper Instance with following parameters: {0}, {1}, {2}".format(group, controllerURL, saIface))
-    wrapperInstance = SAWrapper.Wrapper(group, controllerURL, iface)
-    thread1 = threading.Thread(target=wrapperInstance.keepalive)
-    thread1.setDaemon(True)
-    thread1.start()
-
+    main.wrapperInstance = SAWrapper.Wrapper(main.group, main.controllerURL, main.iface)
+    main.thread1 = threading.Thread(target=main.wrapperInstance.keepalive)
+    main.thread1.setDaemon(True)
+    main.thread1.start()
 
     @app.route('/attack', methods=['POST'])
     def attack():
         # Send Fake Attack Detection Messages with Markov Model.
         # Also works as attack detection message.
-        logger.info("[Attack] Wrapper ready, preparing data...") if wrapperInstance.ready else logger.warning("[Attack] Wrapper Instance not ready! Can't handle attacks from /attack")
-        resp = make_response("Lol")
-        if (wrapperInstance.ready):
-            logger.info("[Attack] Incoming report from Security Appliance {0}".format(str(wrapperInstance.instanceID)))
+        logger.info("[Attack] Wrapper ready, preparing data...") if main.wrapperInstance.ready else logger.warning("[Attack] Wrapper Instance not ready! Can't handle attacks from /attack")
+        if (main.wrapperInstance.ready):
+            logger.info("[Attack] Incoming report from Security Appliance {0}".format(str(main.wrapperInstance.instanceID)))
             # Report Data Structure:
             # { "rate": "20", "misc": "information (???)"}
             try:
@@ -77,14 +75,15 @@ def main(argv):
             # {"type": "ATTACK", "name": "Firewall -1", "group": "firewall", "hw_addr": "00:00:00:00:00:01", "rate": "20",
             #  "token": "secure -token", "misc": "information" }
             logger.info("[Attack] Preparing attack alert to Controller.")
-            attackData = {'type': ApiURI.Type.ATTACK.name, 'name': wrapperInstance.instanceID, 'group': wrapperInstance.group,
-                          'hw_addr': wrapperInstance.iface_mac,
-                          'rate': reportData['rate'], 'token': '', 'misc': ''}
+            attackData = {'type': ApiURI.Type.ATTACK.name, 'name': main.wrapperInstance.instanceID, 'group': main.wrapperInstance.group,
+                          'hw_addr': main.wrapperInstance.iface_mac,
+                          'rate': reportData['rate'], 'misc': ''}
+
             jsonAttackData = json.dumps(attackData)
             logger.info("[Attack] Initializing connection to Controller...")
             connected = False
             while (connected == False):
-                conn = Request(wrapperInstance.controllerURL + ApiURI.Type.ATTACK.value, jsonAttackData.encode("utf-8"),
+                conn = Request(main.wrapperInstance.controllerURL + ApiURI.Type.ATTACK.value, jsonAttackData.encode("utf-8"),
                                {'Content-Type': 'application/json'})
                 attResp = urlopen(conn)
                 print("attResp.getcode(): ", attResp.getcode())
@@ -106,8 +105,23 @@ def main(argv):
         return resp
 
 def sigterm_handler(signal, frame):
+    logger.info("[SIGTERM DELETE] SIGTERM Signal catched. Preparing shutdown...")
+    print(main.wrapperInstance)
     # Send unregister here!
-    print("Got SIGTERM. Bye cruel world...")
+    # Payload: {"type": "DELETE", "name": "instanceID", "misc": "misc info"}
+    payload = {'type': ApiURI.Type.DELETE.name, 'name': str(main.wrapperInstance.instanceID), 'misc':''}
+    print(payload)
+    jsonPayload = json.dumps(payload)
+    delConn = Request(main.controllerURL + ApiURI.Type.DELETE.value,
+                     jsonPayload.encode("utf-8"),
+                     {'Content-Type': 'application/json', 'Authorization': "Bearer {0}".format(main.wrapperInstance.token)})
+    logger.info("[SIGTERM DELETE] Payload prepared. Connecting...")
+    delResp = urlopen(delConn)
+    if(delResp.getcode() == 200):
+        logger.info("[SIGTERM DELETE] Delete Message sent successfully. Shutting down keep-alive thread...")
+        main.wrapperInstance.ready = False
+        main.thread1.join()
+    logger.info("[SIGTERM DELETE] Bye.")
     sys.exit(0)
 
 if(__name__ == "__main__"):
