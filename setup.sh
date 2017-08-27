6ad8788ebca5adf607a2907b8ff65d66498f633c
Hayreddin Ciner <hayreddin.ciner@stud-mail.uni-wuerzburg.de>
2017-08-27 14:35:18 +0200

    SecAppWrapper: -- added Service Support. See Readme.md

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-DIR=/home/eddy/.local/bin/
+DIR=/home/eddy/.local/bin
 
 echo "Installing Security Appliance Wrapper!"
 echo "Copy to user binary directory..."
 mkdir $DIR/SecAppWrapper
-cp SecAppWrapper $DIR/SecAppWrapper/
+cp -R SecAppWrapper $DIR/SecAppWrapper/
 cp startWrapper.py $DIR/SecAppWrapper/
 cp wrapper.ini $DIR/SecAppWrapper/
 echo "Copy done!"
 
 echo "Installing service..."
-cp SAW.service /etc/systemd/user/
+sudo cp SAW.service /etc/systemd/user/
 
 echo "enabling SAW.service!"
 systemctl --user enable SAW.service
