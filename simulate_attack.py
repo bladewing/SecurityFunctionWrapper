016f4605dffe42e1b214956042bf23fbf874a54d
Hayreddin Ciner <hayreddin.ciner@stud-mail.uni-wuerzburg.de>
2017-10-15 19:38:07 +0200

    cleanup

--- simulate_attack.py
+++ simulate_attack.py
@@ -5,29 +5,31 @@ import numpy, time, math, json
 from urllib.request import urlopen, Request
 
 NOTIFY_URL="http://localhost:5001/attack"
-prob=[1/100500,2/100500,3/100500]
+prob=[1/100000,2/100000,3/100000]
 ones = 0
 zeros = 0
 TIMEOUT = time.time() + 600
 template = {"rate":"1", "misc":""}
 data = json.dumps(template)
 i = 0
-count = 1
+count = 0
 while count <=2:
     p = prob[count]
-    while True:
+    i=0
+    print("Probability: ",p)
+    TIMEOUT = time.time() + 600
+    while time.time() < TIMEOUT:
         n = numpy.random.exponential(scale=1.0, size=1)
         if time.time() > TIMEOUT:
             print("TIMEOUT")
             print("ones: ", ones, "percent: ", ones / (ones + zeros))
             print("zeros: ", zeros, "percent: ", zeros / (ones + zeros))
             break
-        elif i >= len(n):
-            i = 0
-        elif n[i] < math.log(1/(1-p)):
+        elif n[0] < math.log(1/(1-p)):
             conn = Request(NOTIFY_URL,data.encode('utf-8'), {'Content-Type': 'application/json'})
             resp = urlopen(conn)
             ones += 1
+            print("Reported")
         else:
             zeros += 1
         i += 1
