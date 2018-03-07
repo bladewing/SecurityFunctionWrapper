4d0312a771a27f4d053137da32b6d5b46a24ee11
Nicolas Fella <nicolas.fella@gmx.de>
2018-03-07 22:34:07 +0100

    Also create logfile when service file generation is skipped

--- setup.sh
+++ setup.sh
@@ -27,13 +27,13 @@ cp start_wrapper.py $DIR/SecAppWrapper/
 cp wrapper.ini $DIR/SecAppWrapper/
 echo "Copy done!"
 
+touch /var/log/SecAppWrapper.log
+
 if [ "$1" != "--nosystemd" ] && [ "$2" != "--nosystemd" ]; then
     echo "Installing service..."
     cp SAW.service.raw SAW.service
     echo 'ExecStart=/usr/bin/python3 /home/'$(whoami)'/bin/SecAppWrapper/start_wrapper.py' >> SAW.service
     sudo cp SAW.service /etc/systemd/user/
-    sudo touch /var/log/SecAppWrapper.log
-    sudo chmod 777 /var/log/SecAppWrapper.log
 
     echo "enabling SAW.service!"
     systemctl --user enable SAW.service
